8fffae68f5394c4c2a450b305612889e0771c934
harry hedges <gk18786@bristol.ac.uk>
2022-02-04 17:23:24 +0000

    made changes -hedges

--- dms.go
+++ dms.go
@@ -18,7 +18,7 @@ func init() {
 }
 
 func main() {
-	PORT := ":8081"
+	PORT := ":8083"
 	http.HandleFunc("/", index)
 	http.HandleFunc("/flip", flop)
 	log.Fatal(http.ListenAndServe(PORT, nil))
@@ -29,16 +29,29 @@ func index(w http.ResponseWriter, r *http.Request) {
 		Title        string
 		Color        string
 		Instructions string
+		Url          string
+		ImgMargin    string
+		Font         string
+		Time         string
 	}{}
 	if kstate == 0 {
-		d.Title = "The Kitchen is Unoccupied"
-		d.Color = "Green"
+		d.Title = "The Kitchen will be occupied by Joe Rogan's chosen people"
+		d.Color = "Gold"
 		d.Instructions = "Click to reserve the kitchen for five minutes"
+		d.Url = "https://i.ytimg.com/vi/u5kfo7MgAtQ/maxresdefault.jpg"
+		d.ImgMargin = "0%"
+		d.Font = "sans-serif"
+		d.Time = "0"
+
 	}
 	if kstate == 1 {
 		d.Title = "The Kitchen is Occupied by Covid Believers"
 		d.Color = "Red"
 		d.Instructions = "You must keep clicking every five minutes"
+		d.Url = "https://i.pinimg.com/originals/13/3d/b4/133db4f9d60cfb7f52c00f8bec546149.png"
+		d.ImgMargin = "-14%"
+		d.Font = "Comic Neue"
+		d.Time = time.Now().Format("01-02-2006 15:04:05")
 	}
 	tmpl.ExecuteTemplate(w, "g.html", d)
 }
