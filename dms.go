c02796eaf70a3c5f53edcdfcc17e1559a268c1e4
Jishnu Kaiwar <jishnukaiwar@gmail.com>
2022-02-05 10:43:39 +0000

    removed kstate

--- dms.go
+++ dms.go
@@ -7,18 +7,12 @@ import (
 	"time"
 )
 
-const siteVerifyURL = "https://www.google.com/recaptcha/api/siteverify"
-
 var tmpl *template.Template
-
-var kstate int = 0
-
-var timestr string = "0"
-
+var timestr string = ""
 var timestrcounter int = 0
 
 func init() {
-	tmpl = template.Must(template.ParseFiles("g.html"))
+	tmpl = template.Must(template.ParseFiles("index.gohtml"))
 }
 
 func main() {
@@ -39,7 +33,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 		Font         string
 		Time         string
 	}{}
-	if kstate == 0 {
+	if timestr == "" {
 		d.Title = "The Kitchen will be occupied by Joe Rogan's chosen people"
 		d.Color = "Gold"
 		d.Instructions = "Click to reserve the kitchen for five minutes"
@@ -48,8 +42,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 		d.Font = "sans-serif"
 		d.Time = "0"
 
-	}
-	if kstate == 1 {
+	} else {
 		d.Title = "The Kitchen is Occupied by Covid Believers"
 		d.Color = "Red"
 		d.Instructions = "You must keep clicking every five minutes"
@@ -58,33 +51,28 @@ func index(w http.ResponseWriter, r *http.Request) {
 		d.Font = "Comic Neue"
 		d.Time = timestr
 	}
-	tmpl.ExecuteTemplate(w, "g.html", d)
+	tmpl.ExecuteTemplate(w, "index.gohtml", d)
+}
+
+func fire() {
+	timestrcounter = timestrcounter - 1
+	if timestrcounter == 0 {
+		timestr = ""
+	}
 }
 
 func flop(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
-	kstate = 1
 	timestr = time.Now().Format("01-02-2006 15:04:05")
 	timestrcounter = timestrcounter + 1
-	dur := time.Duration(5) * time.Minute
-	f := func() {
-		timestrcounter = timestrcounter - 1
-		if timestrcounter == 0 {
-			kstate = 0
-		}
-	}
-	time.AfterFunc(dur, f)
+	dur := time.Duration(10) * time.Second
+	time.AfterFunc(dur, fire)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func okcharles(w http.ResponseWriter, r *http.Request) {
-	if kstate == 0 {
+	if timestr == "" {
 		w.Write([]byte("novid: false"))
-	}
-	if kstate == 1 {
+	} else {
 		w.Write([]byte("novid: true"))
 	}
 }
