369c920a186f0da26db0725dd0b534710f2e10af
Jishnu Kaiwar <jishnukaiwar@gmail.com>
2022-09-18 17:09:14 +0100

    cleaned up

--- dms.go
+++ dms.go
@@ -41,20 +41,16 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		Time         string
 	}{}
 	if timestr == "" {
-		d.Title = "The Kitchen will be occupied by Joe Rogan's chosen people"
+		d.Title = "The Kitchen is Free"
 		d.Color = "Gold"
 		d.Instructions = "Click to reserve the kitchen for five minutes"
-		d.Url = "https://i.ytimg.com/vi/u5kfo7MgAtQ/maxresdefault.jpg"
-		d.ImgMargin = "0%"
 		d.Font = "sans-serif"
 		d.Time = "0"
 
 	} else {
-		d.Title = "The Kitchen is Occupied by Covid Believers"
+		d.Title = "The Kitchen is Occupied"
 		d.Color = "Red"
-		d.Instructions = "You must keep clicking every five minutes"
-		d.Url = "https://i.pinimg.com/originals/13/3d/b4/133db4f9d60cfb7f52c00f8bec546149.png"
-		d.ImgMargin = "-14%"
+		d.Instructions = "Reservation expiring soon"
 		d.Font = "Comic Neue"
 		d.Time = timestr
 	}
